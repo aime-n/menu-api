9cf7c0bd23a09a4c7d17bd504b6a9cf4ae959535
aime-n <aime.nobrega@gmail.com>
2025-06-17 01:29:18 -0300

    testar e consertar organizacao

---
+++ scripts/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Atualiza dependências
+poetry install
+
+# Formata o código
+poetry run isort tests/
+poetry run black tests/
+
+# Cria diretório de migrações Alembic se não existir
+if [ ! -d "app/db/migration/versions" ]; then
+  poetry run alembic -c alembic.ini init app/db/migration
+fi
+
+# Aplica as migrações pendentes antes de gerar nova revisão
+poetry run alembic -c alembic.ini upgrade head
+
+# Gera uma nova revisão Alembic com autogenerate
+poetry run alembic -c alembic.ini revision --autogenerate -m "update"
+
+# Aplica as migrações no banco de dados
+poetry run alembic -c alembic.ini upgrade head
+
+# Roda os testes automatizados
+poetry run pytest
