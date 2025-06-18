979f591b38f5e5ffe297ff2109e43a9fbb5da0af
aime-n <aime.nobrega@gmail.com>
2025-06-18 00:15:29 -0300

    add bandit

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,7 +6,6 @@ poetry run mypy --ignore-missing-imports app/
 
 poetry run black --check app/ tests/
 poetry run flake8 app/ tests/
-poetry run safety check -i 39462 -i 40291
 poetry run bandit -r app/
 poetry run mypy app/
 poetry run flake8 app/
